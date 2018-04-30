eff98c7e95094bd4ee4a2a1096d51113bab1b441
DailyAlice <mara.catherine@gmail.com>
2018-04-30 14:46:46 -0500

    adding no-database example

---
+++ example/example.rb
@@ -0,0 +1,23 @@
+#example.rb
+require 'rubygems'
+require 'bundler/setup'
+require 'sinatra'
+
+all_words = []
+
+get '/' do
+    erb :index
+end
+
+
+put '/words' do
+    new_word = {"word" => params[:word], "definition" => params[:definition]}
+    all_words.push(new_word)
+    return [204, ['']]
+end
+
+
+get '/words' do
+    content_type :json
+    return all_words.to_json
+end
